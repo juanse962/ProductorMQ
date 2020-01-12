62fa5575b2148637cedc9a6cb86ad3e18a805c2d
juanse962 <juanseg11@hotmail.com>
2020-01-12 16:34:28 -0500

    Agregar archivos de proyecto.

---
+++ ProductorMQ/WebForm1.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ProductorMQ.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Seleccione cola<br />
+            <asp:DropDownList ID="DropDownList1" runat="server">
+                <asp:ListItem>cola1</asp:ListItem>
+                <asp:ListItem>cola2</asp:ListItem>
+            </asp:DropDownList>
+            <br />
+            <br />
+            Ingrese mensaje
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Enviar" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ProductorMQ/WebForm1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProductorMQ
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection()) {
+                using (var channel = connection.CreateModel()) {
+                    String cola = DropDownList1.SelectedItem.ToString();
+                    channel.QueueDeclare(cola, false, false, false, null);
+                    string message = TextBox1.Text;
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish("",cola, basicProperties: null, body: body);
+
+                }
+            }
+        }
+    }
+}
